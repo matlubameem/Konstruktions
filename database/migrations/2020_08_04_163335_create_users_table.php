5ebfa33f66ca60cd315930e2830a7af8170539a9
matlubameem <meem.matluba@gmai.com>
2020-08-05 22:56:26 +0600

    User and Role Migration done and next is business logic for login system

--- database/migrations/2020_08_04_163335_create_users_table.php
+++ database/migrations/2020_08_04_163335_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id')->constrained('roles')->onDelete('cascade');
+            $table->foreignId('role_id')->constrained('roles')->onDelete('cascade');
             $table->string('father_name',25);
             $table->string('first_name',10);
             $table->string('last_name',10);
@@ -28,7 +28,13 @@ class CreateUsersTable extends Migration
             $table->string('image',21)->nullable();
             $table->string('section',10)->nullable();
             $table->float('salary',10,2)->nullable();
+            $table->string('ip_address',55);
+            $table->string('last_login',55);
             $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->tinyInteger('email_verify')->nullable();
+            $table->tinyInteger('phone_verify')->nullable();
+            $table->string('sms_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
